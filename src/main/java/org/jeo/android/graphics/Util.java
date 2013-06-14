669f41ec0d3f95fcca9fffa38755630fc131ba8e
jdeolive <jdeolive@opengeo.org>
2013-06-14 08:07:39 -0600

    made utility class public

--- src/main/java/org/jeo/android/graphics/Util.java
+++ src/main/java/org/jeo/android/graphics/Util.java
@@ -7,9 +7,9 @@ import android.graphics.RectF;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 
-class Util {
+public class Util {
 
-    static Rect expand(Rect r, float val) {
+    public static Rect expand(Rect r, float val) {
         Rect n = new Rect(r);
     
         n.top -= val;
@@ -20,41 +20,45 @@ class Util {
         return n;
     }
     
-    static RectF rectFromCenter(PointF p, float width, float height) {
+    public static RectF rectFromCenter(PointF p, float width, float height) {
         float left = p.x - width/2f;
         float top = p.y - height/2f;
 
         return new RectF(left, top, left + width, top + height);
     }
     
-    static RectF rectFromBottomLeft(PointF p, float width, float height) {
+    public static RectF rectFromBottomLeft(PointF p, float width, float height) {
         float left = p.x;
         float top = p.y + height;
         
         return new RectF(left, top, left + width, p.y);
     }
 
-    static RectF rectFromBottomCenter(PointF p, float width, float height) {
+    public static RectF rectFromBottomCenter(PointF p, float width, float height) {
         float left = p.x - width/2f;
         float top = p.y + height;
 
         return new RectF(left, top, left + width, p.y);
     }
 
-    static RectF rectFromBottomRight(PointF p, float width, float height) {
+    public static RectF rectFromBottomRight(PointF p, float width, float height) {
         float left = p.x - width;
         float top = p.y + height;
 
         return new RectF(left, top, p.x, p.y);
     }
 
-    static Envelope envelope(RectF rect) {
+    public static Envelope envelope(RectF rect) {
         //double h = map.getHeight();
         //return new Envelope(rect.left, rect.right, h - rect.top, h - rect.bottom);
         return new Envelope(rect.left, rect.right, rect.bottom, rect.top);
     }
 
-    static PointF point(Coordinate c) {
+    public static RectF rect(Envelope e) {
+        return new RectF((float)e.getMinX(), (float)e.getMaxY(), (float)e.getMaxX(), (float) e.getMinY());
+    }
+    
+    public static PointF point(Coordinate c) {
         return new PointF((float) c.x, (float) c.y);
     }
 }
