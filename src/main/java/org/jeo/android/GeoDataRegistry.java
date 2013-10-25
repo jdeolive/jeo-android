d0417c365b28852e8e94dcbf5bf467262bd16df5
jdeolive <jdeolive@boundlessgeo.com>
2013-10-25 07:55:08 -0600

    updated GeoDataRegistry to support both index.json or directory mode

--- src/main/java/org/jeo/android/GeoDataRegistry.java
+++ src/main/java/org/jeo/android/GeoDataRegistry.java
@@ -1,12 +1,15 @@
 package org.jeo.android;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.jeo.android.geopkg.GeoPackage;
 import org.jeo.android.mbtiles.MBTiles;
+import org.jeo.data.DataRef;
 import org.jeo.data.DirectoryRegistry;
 import org.jeo.data.DriverRegistry;
-
+import org.jeo.data.JSONRegistry;
+import org.jeo.data.Registry;
 import org.jeo.data.StaticDriverRegistry;
 import org.jeo.data.mem.Memory;
 import org.jeo.geojson.GeoJSON;
@@ -14,15 +17,24 @@ import org.jeo.geojson.GeoJSON;
 import android.os.Environment;
 
 /**
- * Data registry that exposes data from a directory named "GeoData" on the SDCard 
- * of the device, obtained from {@link Environment#getExternalStorageDirectory()}.
- * 
+ * Data registry that exposes data from a directory.
+ * <p>
+ * If not overridden by the user the default directory is named "GeoData" on the root of the SDCard
+ * of the device, obtained from {@link Environment#getExternalStorageDirectory()}. 
+ * </p>
+ * <p>
+ * The registry will operate in one of two modes. If the specified data directory contains an 
+ * <tt>index.json</tt> file it will operate as {@link JSONRegistry}. Otherwise it will simply scan 
+ * the directory contains as {@link DirectoryRegistry}.
+ * </p>
  * @author Justin Deoliveira, OpenGeo
  */
-public class GeoDataRegistry extends DirectoryRegistry {
+public class GeoDataRegistry implements Registry {
 
     static DriverRegistry DRIVERS = 
         new StaticDriverRegistry(new GeoPackage(), new MBTiles(), new GeoJSON(), new Memory());
+
+    Registry delegate;
 
     /**
      * Returns the GeoData directory handle.
@@ -32,10 +44,47 @@ public class GeoDataRegistry extends DirectoryRegistry {
     }
 
     public GeoDataRegistry() {
-        this(DRIVERS); 
+        this(null); 
     }
 
     public GeoDataRegistry(DriverRegistry drivers) {
-        super(directory(), drivers);
+        this(null, drivers);
+    }
+
+    public GeoDataRegistry(File dir, DriverRegistry drivers) {
+        if (dir == null) {
+            dir = directory();
+        }
+
+        if (drivers == null) {
+            drivers = DRIVERS;
+        }
+
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException("not a directory: " + dir.getPath());
+        }
+
+        File index = new File(dir, "index.json");
+        if (index.exists()) {
+            delegate = new JSONRegistry(index, DRIVERS);
+        }
+        else {
+            delegate = new DirectoryRegistry(dir, DRIVERS);
+        }
+    }
+
+    @Override
+    public Iterable<DataRef<?>> list() throws IOException {
+        return delegate.list();
+    }
+
+    @Override
+    public Object get(String name) throws IOException {
+        return delegate.get(name);
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
     }
 }
